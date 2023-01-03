ad00cb03ed0e1c100903b5e427fde5fa37fabd6d
patrick.Z <775150@qq.com>
2023-01-03 22:07:41 +0800

    doc: comment

--- get_rx_persist/lib/src/get_rx_module.dart
+++ get_rx_persist/lib/src/get_rx_module.dart
@@ -3,6 +3,9 @@ import 'dart:async';
 import 'package:get_module/get_module.dart';
 import 'package:get_rx_persist/get_rx_persist.dart';
 
+/// GetRxModule
+///
+/// add [Module] support for get_module
 class GetRxModule extends Module {
   @override
   Future<void> install() async {
